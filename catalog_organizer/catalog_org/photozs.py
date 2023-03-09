bebeb823859c15f4e41d9e62c9b97ec757daafac
Paula Ferreira <psilf12@gmail.com>
2023-03-09 09:30:06 -0300

    mar9

--- catalog_organizer/catalog_org/photozs.py
+++ catalog_organizer/catalog_org/photozs.py
@@ -15,11 +15,10 @@ def handling_photoz(path,output_file):
                 cols=y.strip()
                 lista.append(cols)
     bpz_photoz=[lista[i].split(' ') for i in range(len(lista))]
-    iden=np.array([float(bpz_photoz[i][0]) for i in range(len(bpz_photoz))])
     photoz=np.array([float(bpz_photoz[i][1]) for i in range(len(bpz_photoz))])
     z_bmin=np.array([float(bpz_photoz[i][2]) for i in range(len(bpz_photoz))])
     z_bmax=np.array([float(bpz_photoz[i][5]) for i in range(len(bpz_photoz))])
-    np.save(output_file,np.column_stack((iden,photoz,z_bmin,z_bmax)))
+    np.save(output_file,np.column_stack((photoz,z_bmin,z_bmax)))
     
     return
 
---
+++ catalog_organizer/catalog_org/redshift_distribution.py
@@ -0,0 +1,24 @@
+from astropy.io import fits
+import numpy as np
+import os
+import random
+from astropy.table import Table
+import time 
+
+def random_selector(data,data2,dist):
+
+    start = time.time()
+    new=[i for i in range(len(data2)) if data2[i]==data[random.choice(dist)]]
+
+    end = time.time()
+    print(end - start)
+    return new
+
+def new_file(data2,new_ind,output):
+	new={}
+	for i in new_ind:
+		new['Z']=data2['Z'][i]
+		new['RA']=data2['RA'][i]
+		new['DEC']=data2['DEC'][i]
+	Table(new).write(output,overwrite=True)
+	return
